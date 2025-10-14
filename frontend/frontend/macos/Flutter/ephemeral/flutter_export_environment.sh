b0d7f6c7e8580a5e43777718b33c51c1b5b11230
mingnhi <luongbaotam2905@gmail.com>
2025-10-15 01:14:20 +0700

    Update Flutter environment configuration paths and remove obsolete plugin symlinks

--- frontend/frontend/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ frontend/frontend/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=E:\flutter install\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\flutter2\Booking-app\frontend\frontend"
+export "FLUTTER_ROOT=D:\app\flutter_windows_3.29.3-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\devops\myapp\frontend\frontend"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
